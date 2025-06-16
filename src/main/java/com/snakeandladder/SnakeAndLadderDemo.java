0bacae6a7d19d4fe68f63cd89d86d8a1681026df
rajkumar <raj.kumar@nobroker.in>
2025-06-16 16:21:49 +0530

    code cleanup

--- src/main/java/com/snakeandladder/SnakeAndLadderDemo.java
+++ src/main/java/com/snakeandladder/SnakeAndLadderDemo.java
@@ -5,22 +5,20 @@ import java.util.List;
 
 public class SnakeAndLadderDemo {
     public static void main(String args[]) {
-        Board board = new Board("B1", 100);
-        Match match = new Match("M1");
-        Dice dice = new Dice(1, 6);
-        int numOfPlayer = 2;
-        List<Player> players = SnakeAndLadderGame.generatePlayers(numOfPlayer);
-        List<Snake> snakes = SnakeAndLadderGame.generateSnakes(new int[][]{{10, 7}, {98, 54}, {50, 40}, {99, 1}});
-        List<Ladder> ladders = SnakeAndLadderGame.generateLadders(new int[][]{{6, 11}, {5, 54}, {17, 88}, {2, 99}});
+        Board board = new Board("Board_1", 100);
+        Match match = new Match("Match_1");
 
-        board.setLadders(ladders);
-        board.setSnakes(snakes);
+        List<Player> players = SnakeAndLadderGame.createPlayers(2);
+        board.setSnakes(SnakeAndLadderGame.createSnakes());
+        board.setLadders(SnakeAndLadderGame.createLadders());
+
+        Dice dice = new Dice(1, 6);
+
+        match.setBoard(board);
         match.setPlayerList(players);
         match.setDice(dice);
-        match.setBoard(board);
 
         SnakeAndLadderGame.play(match);
-        System.out.println("Winner : " + match.getWinner().getId());
 
     }
 }
--- src/main/java/com/snakeandladder/SnakeAndLadderGame.java
+++ src/main/java/com/snakeandladder/SnakeAndLadderGame.java
@@ -4,72 +4,106 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SnakeAndLadderGame {
-    public static List<Player> generatePlayers(int numOfPlayers) {
-        List<Player> list = new ArrayList<>();
-        for (int i = 0; i < numOfPlayers; i++) {
-            list.add(new Player("" + i, " name_" + i));
-        }
-        return list;
-    }
-
-    public static List<Snake> generateSnakes(int[][] snakes) {
-        List<Snake> list = new ArrayList<>();
-        for (int i = 0; i < snakes.length; i++) {
-            list.add(new Snake(snakes[i][0], snakes[i][1]));
-        }
-        return list;
-    }
-
-    public static List<Ladder> generateLadders(int[][] ladders) {
-        List<Ladder> list = new ArrayList<>();
-        for (int i = 0; i < ladders.length; i++) {
-            list.add(new Ladder(ladders[i][0], ladders[i][1]));
-        }
-        return list;
-    }
-
     public static void play(Match match) {
         match.setMatchStatus(MatchStatus.IN_PROGRESS);
-        for (int i = 0; i < match.getPlayerList().size(); i++) {
-            Player player = match.getPlayerList().get(i);
-            match.getBoard().getPlayerPositionHashMap().put(player, -1);
-        }
-        while (match.getMatchStatus() != MatchStatus.ENDED) {
-            for (int i = 0; i < match.getPlayerList().size(); i++) {
-                if (match.getMatchStatus() == MatchStatus.ENDED) {
-                    break;
-                }
-                Player player = match.getPlayerList().get(i);
-                int currentPos = match.getBoard().getPlayerPositionHashMap().get(player);
-                int nextPos = match.getDice().play();
+        initializePlayers(match);
 
-                if (currentPos == -1 && nextPos != 1) {
-                    System.out.println("Player not in board yet , he needs to put 1 to continue : " + player.getId());
+        while (match.getMatchStatus() != MatchStatus.ENDED) {
+            for (Player player : match.getPlayerList()) {
+                if (match.getMatchStatus() == MatchStatus.ENDED) break;
+
+                int roll = match.getDice().play();
+                int currentPos = match.getBoard().getPlayerPositionHashMap().get(player);
+                int newPos = computeNextPosition(currentPos, roll);
+
+                if (!isValidMove(currentPos, roll, match.getBoard().getMaxPos())) {
+                    log(player.getPlayerName() + " rolled " + roll + " but can't move.");
                     continue;
                 }
-                nextPos = nextPos + currentPos;
-                if (nextPos > match.getBoard().getMaxPos()) {
-                    continue;
-                }
-                for (Snake snake : match.getBoard().getSnakes()) {
-                    if (snake.getHead() == nextPos) {
-                        nextPos = snake.getTail();
-                        break;
-                    }
-                }
-                for (Ladder ladder : match.getBoard().getLadders()) {
-                    if (ladder.getStart() == nextPos) {
-                        nextPos = ladder.getEnd();
-                        break;
-                    }
-                }
-                match.getBoard().getPlayerPositionHashMap().put(player, nextPos);
-                System.out.println("Player " + player.getId() + " going to : " + nextPos);
-                if (nextPos == match.getBoard().getMaxPos()) {
+
+                newPos = applySnakesAndLadders(newPos, match.getBoard());
+
+                match.getBoard().getPlayerPositionHashMap().put(player, newPos);
+                log(player.getPlayerName() + " rolled " + roll + " and moved to " + newPos);
+
+                if (newPos == match.getBoard().getMaxPos()) {
                     match.setMatchStatus(MatchStatus.ENDED);
                     match.setWinner(player);
+                    log("🎉 " + player.getPlayerName() + " has won the game!");
+                    break;
                 }
             }
         }
     }
+
+// ---------------- Helper Methods ---------------- //
+
+    private static void initializePlayers(Match match) {
+        for (Player player : match.getPlayerList()) {
+            match.getBoard().getPlayerPositionHashMap().put(player, -1);  // Not entered yet
+        }
+    }
+
+    private static int computeNextPosition(int currentPos, int roll) {
+        return (currentPos == -1) ? ((roll == 1) ? 1 : -1) : currentPos + roll;
+    }
+
+    private static boolean isValidMove(int currentPos, int roll, int maxPos) {
+        if (currentPos == -1 && roll != 1) return false; // Must roll 1 to enter
+        return (currentPos + roll) <= maxPos;
+    }
+
+    private static int applySnakesAndLadders(int pos, Board board) {
+        for (Snake snake : board.getSnakes()) {
+            if (snake.getHead() == pos) {
+                log("🐍 Bitten by snake! Down to " + snake.getTail());
+                return snake.getTail();
+            }
+        }
+
+        for (Ladder ladder : board.getLadders()) {
+            if (ladder.getStart() == pos) {
+                log("🪜 Climbed a ladder! Up to " + ladder.getEnd());
+                return ladder.getEnd();
+            }
+        }
+
+        return pos;
+    }
+
+    private static void log(String msg) {
+        System.out.println(msg);
+    }
+
+    public static List<Player> createPlayers(int n) {
+        List<Player> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            list.add(new Player("P" + i, "Player_" + i));
+        }
+        return list;
+    }
+
+    public static List<Snake> createSnakes() {
+        return List.of(
+                new Snake(99, 78),
+                new Snake(95, 56),
+                new Snake(87, 24),
+                new Snake(62, 18),
+                new Snake(48, 26),
+                new Snake(36, 6)
+        );
+    }
+
+    public static List<Ladder> createLadders() {
+        return List.of(
+                new Ladder(2, 23),
+                new Ladder(8, 34),
+                new Ladder(20, 77),
+                new Ladder(32, 68),
+                new Ladder(41, 79),
+                new Ladder(74, 92)
+        );
+    }
+
+
 }
